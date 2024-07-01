1f663d046db9423a56b5a2098ee5fd137d2e9217
Arnaldo Pérez <arnaldoeperezb@gmail.com>
2024-06-30 20:23:17 -0400

    Error handle

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ app.get("/",(req,res)=>{
 app.get("/:username",async (req,res)=>{
     let {username}=req.params
     // https://api.freecodecamp.org/api/users/get-public-profile?username=keinny25
-    let resp=await fetch("https://api.freecodecamp.org/api/users/get-public-profile?username="+username)
-    let data=await resp.json()
-    return res.status(200).send({username,data})
+    try {
+        let resp=await fetch("https://api.freecodecamp.org/api/users/get-public-profile?username="+username)
+        let data=await resp.json()
+        return res.status(200).send(data)        
+    } catch (error) {
+        return res.status(401).send(error)
+    }
 })
 
 app.listen(3000,()=>{console.log("Listeing on 3000")})
